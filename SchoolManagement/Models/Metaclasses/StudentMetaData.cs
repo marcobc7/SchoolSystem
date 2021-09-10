017fc2714024c4577f7c3158953cc78ca6592031
marcovm <marcovm@MarcoVM>
2021-09-10 00:56:07 +0000

    Add project files.

---
+++ SchoolManagement/Models/Metaclasses/StudentMetaData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.Models
+{
+    public class StudentMetaData
+    {
+        [StringLength(50)]
+        [Display(Name ="Last Name")]
+        public string LastName { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+    }
+
+    [MetadataType(typeof(StudentMetaData))]
+    public partial class Student
+    {
+
+    }
+
+
+}
